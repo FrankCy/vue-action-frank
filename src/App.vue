8d6d09e711a9c813a9f699a2f843a3419f77e6c5
yang.chang <cy880708@163.com>
2021-08-09 17:10:01 +0800

    vue demo

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,67 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    data() {
+      return {
+        msg_data: []
+      }
+    },
+    created() {
+      this.initWebSocket();
+      // this.initWebSocket2();
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    },
+    destroyed: function () {
+      this.websocketclose();
+    },
+    methods: {
+      initWebSocket2: function () {
+        this.websock2 = new WebSocket("ws://localhost:8085/metricsDetailServer/vue_detail_endpoint");
+        this.websock2.onopen = this.websocketonopen;
+        this.websock2.onerror = this.websocketonerror;
+        this.websock2.onmessage = this.websocketonmessage;
+        this.websock2.onclose = this.websocketclose;
+      },
+      websocketonopen: function () {
+        console.log("WebSocket连接成功");
+      },
+      websocketonerror: function (e) {
+        console.log(e)
+        console.log("WebSocket连接发生错误");
+      },
+      websocketonmessage: function (e) {
+        var da = JSON.parse(e.data);
+        console.log(da);
+        this.msg_data.unshift(da);
+      },
+      websocketclose: function (e) {
+        console.log("connection closed (" + e.code + ")");
+      },
+      initWebSocket: function () {
+        this.websock = new WebSocket("ws://localhost:8085/metricsMainServer/vue_main_endpoint");
+        this.websock.onopen = this.websocketonopen2;
+        this.websock.onerror = this.websocketonerror2;
+        this.websock.onmessage = this.websocketonmessage2;
+        this.websock.onclose = this.websocketclose2;
+      },
+      websocketonopen2: function () {
+        console.log("WebSocket连接成功");
+      },
+      websocketonerror2: function (e) {
+        console.log(e)
+        console.log("WebSocket连接发生错误");
+      },
+      websocketonmessage2: function (e) {
+        var da = JSON.parse(e.data);
+        console.log(da);
+        this.msg_data.unshift(da);
+      },
+      websocketclose2: function (e) {
+        console.log("connection closed (" + e.code + ")");
+      }
+    }
   }
-}
 </script>
 
 <style>
